fdd607b7d7216c1f212559f6043ce8073ccea95d
Daryl John <79010707+Avaitor777@users.noreply.github.com>
2024-01-30 23:32:51 +0530

    page4 updation(geographic visualization)
    use of nivo codes and features- imported world map from nivo-linked to user information-defined country format(server)-  imported countryISO2to3(since default format is 3, need to convert to 2)

--- server/index.js
+++ server/index.js
@@ -16,7 +16,13 @@ import User from "./models/User.js";
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import OverallStat from './models/OverallStat.js';
+import { dataUser, 
+    dataProduct, 
+    dataProductStat, 
+    dataTransaction,
+    dataOverallStat, //2021 is only put to make it comprehensive, modify
+} from "./data/index.js";
 
 
 
@@ -52,7 +58,7 @@ mongoose.connect(process.env.MONGO_URL, {
         //Product.insertMany(dataProduct);
         //ProductStat.insertMany(dataProductStat);
         //Transaction.insertMany(dataTransaction);
+        //OverallStat.insertMany(dataOverallStat);
         
-
     })
     .catch((error) => console.error(`Error connecting to MongoDB: ${error.message}`));
